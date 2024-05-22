b3813a8fdb252f40021405e3c1c0ad3b3868f97f
kollisey1418 <41780276+kollisey1418@users.noreply.github.com>
2024-05-22 20:09:43 +0200

    Исправлен файл main/urls.py, завершив определение пути для edit_item.

--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('warehouse-new/', views.warehouse_new, name='warehouse_new'),
     path('add-item/', views.add_item, name='add_item'),
     path('delete-item/', views.delete_item, name='delete_item'),
-    path('edit
+    path('edit-item/', views.edit_item, name='edit_item'),  # Завершаем определение пути
+]
